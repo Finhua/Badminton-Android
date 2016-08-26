1b540872a034ab04537ef9e6daa92784716f0540
Chao Wei <chao.wei.fi@gmail.com>
2016-08-26 23:10:35 +0300

    fixes app crash

--- app/src/main/java/fi/finhua/android/badminton/MainActivity.java
+++ app/src/main/java/fi/finhua/android/badminton/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
